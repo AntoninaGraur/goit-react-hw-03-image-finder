90f274862861bb22fc83b8af35bed262eb1abc78
Antonina Graur <124353835+AntoninaGraur@users.noreply.github.com>
2023-06-04 15:24:45 +0200

    ready

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,25 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+import Searchbar from './SearchBar/SearchBar';
+import ImageGallery from './ImageGallery/ImageGallery';
+
+export class App extends Component {
+  state = {
+    searchImage: '',
+  };
+
+  handleFormSubmit = searchImage => {
+    this.setState({ searchImage });
+  };
+
+  render() {
+    const { searchImage } = this.state;
+    return (
+      <div>
+        <Searchbar onSubmit={this.handleFormSubmit} />
+        <ImageGallery searchImage={searchImage} />
+      </div>
+    );
+  }
+}
+
+export default App;
